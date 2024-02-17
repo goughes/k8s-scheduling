55326dd6fd958dc9ae470fd08771e9dfef439047
Erik Gough <goughes@purdue.edu>
2024-02-17 13:04:35 -0500

    Add capacity and yunikorn files

---
+++ capacity/create_tenant_ns.sh
@@ -0,0 +1,4 @@
+kubectl create ns tenant1
+kubectl create ns tenant2
+kubectl create ns tenant3
+kubectl create ns tenant4
---
+++ capacity/delete_tenant_ns.sh
@@ -0,0 +1,4 @@
+kubectl delete ns tenant1
+kubectl delete ns tenant2
+kubectl delete ns tenant3
+kubectl delete ns tenant4
