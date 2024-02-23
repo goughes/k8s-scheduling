e609c2b506dcd543b8b097627e5ffa0ed2c78c75
Erik Gough <goughes@purdue.edu>
2024-02-23 13:12:53 -0500

    Clean up capacity configs

--- capacity/delete_tenant_ns.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete ns tenant1
-kubectl delete ns tenant2
-kubectl delete ns tenant3
-kubectl delete ns tenant4
--- capacity/reinstall-scheduler-plugins.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl delete eq --all --all-namespaces
-kubectl delete crd appgroups.appgroup.diktyo.x-k8s.io elasticquotas.scheduling.x-k8s.io networktopologies.networktopology.diktyo.x-k8s.io noderesourcetopologies.topology.node.k8s.io podgroups.scheduling.x-k8s.io
-
-helm -n scheduler-plugins uninstall scheduler-plugins
-kubectl delete ns scheduler-plugins
-sleep 1
-helm install scheduler-plugins ~/scheduler-plugins/manifests/install/charts/as-a-second-scheduler --create-namespace --namespace scheduler-plugins
